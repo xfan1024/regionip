4ca089c00ffcb7ff080f7a30fe236293b20a6ad7
xfan1024 <xfan1024@live.com>
2021-01-01 18:36:11 +0800

    add script to generate some rule
    Signed-off-by: xfan1024 <xfan1024@live.com>

---
+++ update-rule.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+dnsmasq_china_list_file_url=https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+
+
+generate_dns_rule_head(){
+echo '[outland]
+server=${outland_dns}
+*
+
+[inland]
+server=${mainland_dns}
+${direct_list}'
+}
+
+
+generate_dns_rule(){
+    echo write head to "$1" ... && generate_dns_rule_head >"$1"
+    echo download data and write to "$1" ... && curl $dnsmasq_china_list_file_url | cut -d/ -f2 >> "$1"
+    echo generate "$1".gz ... && gzip -fk "$1"
+}
+
+generate_ip_rule(){
+    echo update apnic-latest.txt ... && ./regionip.py --update
+    echo write to "$1" ... && ./regionip.py CN >"$1"
+    echo generate "$1".gz ... && gzip -fk "$1"
+}
+
+generate_dns_rule dns_rule
+generate_ip_rule ip_rule

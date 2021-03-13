ee4562f98797241b504d6f9cee924cebb8df1622
xfan1024 <xfan1024@live.com>
2021-03-13 06:34:06 +0000

    update greatwall.sh
    Signed-off-by: xfan1024 <xfan1024@live.com>

--- greatwall.sh
+++ greatwall.sh
@@ -12,12 +12,17 @@ ipt_chain=greatwall
 generate_ipset_restore_content()
 {
 	echo -N $set_name nethash
+	awk '{print "add '$set_name' "$1}' <<EOF
+10.0.0.0/8
+172.16.0.0/12
+192.168.0.0/16
+EOF
 	awk '{print "add '$set_name' "$1}' <"$ip_rule"
 }
 
 _disable_greatwall()
 {
-	iptables -D INPUT -j $ipt_chain || true
+	iptables -D INPUT ! -i lo -j $ipt_chain || true
 	iptables -F $ipt_chain || true
 	iptables -X $ipt_chain || true
 	ipset destroy $set_name || true
@@ -25,7 +30,7 @@ _disable_greatwall()
 
 disable_greatwall()
 {
-	_disable_greatwall >/dev/null
+	_disable_greatwall 2>/dev/null
 }
 
 enable_greatwall()
@@ -35,14 +40,14 @@ enable_greatwall()
 	iptables-restore <<EOF
 *filter
 -N $ipt_chain
--A $ipt_chain -m set --match-set $set_name src -j ACCEPT
+-A $ipt_chain -m set --match-set $set_name src -j RETURN
 -A $ipt_chain -j DROP
--A INPUT -j $ipt_chain
+-A INPUT ! -i lo -j $ipt_chain
 COMMIT
 EOF
 }
 
-case $1 in 
+case $1 in
 	enable)
 		enable_greatwall
 		;;
